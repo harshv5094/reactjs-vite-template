3d9f2532b96384cb5ce302f0c4705dff7977e5d6
Harsh Vyapari <harshv1741@gmail.com>
2023-07-09 11:15:14 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Harsh Vyapari</h1>
+      <h2>React JS Template</h2>
+    </div>
+  );
+}
+
+export default App;
