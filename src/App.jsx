b4aca1d79573f6ffade7202385f3da572eb64bb5
Harsh Vyapari <harshv1741@gmail.com>
2023-08-01 22:57:06 +0530

    feat: Adding Tailwind CSS in vite template

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Harsh Vyapari</h1>
-      <h2>React JS Template</h2>
+    <div className="text-center">
+      <h1 className="font-bold text-3xl m-5">Harsh Vyapari</h1>
+      <h2 className="font-bold text-3xl m-5">React JS Template with tailwindcss</h2>
     </div>
   );
 }
