2b50d24f48fea80e02262f6c347241902f811501
Harsh Vyapari <harshv1741@gmail.com>
2023-08-01 23:02:55 +0530

    Auto stash before rebase of "refs/heads/main"

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  tabWidth: 2,
+  bracketSpacing: true,
+  semi: false,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  trailingComma: "none",
+  arrowParens: "avoid",
+  endOfLine: "lf",
+  printWidth: 80,
+  plugins: ['prettier-plugin-tailwindcss'],
+}
